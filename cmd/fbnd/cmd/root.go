21c62f35f619dc03111fe09393125de8ae0069ba
Niklas Vogel <21263864+n9v9@users.noreply.github.com>
2021-01-25 18:29:27 +0100

    Add version flag

--- cmd/fbnd/cmd/root.go
+++ cmd/fbnd/cmd/root.go
@@ -8,14 +8,21 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// Values for these variables get injected by the go linker for every release.
+var (
+	version  string = "development"
+	commitID string
+)
+
 // Global flags that are set for all commands.
 var (
 	outputJSON = false
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "fbnd",
-	Short: "Timetables of FB03 inside your terminal",
+	Use:     "fbnd",
+	Version: versionString(),
+	Short:   "Timetables of FB03 inside your terminal",
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 		noColor, err := cmd.Flags().GetBool("no-color")
 		if err != nil {
@@ -36,4 +43,11 @@ func Execute() {
 func init() {
 	rootCmd.PersistentFlags().BoolVar(&outputJSON, "json", false, "Enable printing results in JSON format")
 	rootCmd.PersistentFlags().Bool("no-color", false, "Disable colorized output")
+}
+
+func versionString() string {
+	if commitID == "" {
+		return version
+	}
+	return version + " " + commitID
 }
